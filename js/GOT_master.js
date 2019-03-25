632d0cb9d60878070e82c90727a5c5b992b730dc
Khalifa <m_khalifa@tss-hr420-dm15.fc.ca>
2019-03-25 15:18:00 -0400

    round1

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -8,6 +8,10 @@
 			video		= document.querySelector('video');
 			closeLB		= document.querySelector('.close-lightbox');
 			banners 	= document.querySelector('#houseImages');
+			houseName 	= document.querySelector('.house-name');
+			houseInfo 	= document.querySelector('.house-info');
+			houseData 	= [//STARK
+`House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`];	
 
 	function showLightbox() {
 		//grab the right video source
@@ -17,6 +21,12 @@
 
 		//make sure the names match - needs to be upper case
 		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		//replace text in html
+		////the backtics are called js template strings
+		houseName.textContent = `House ${targetSrc}`;
+		//this only even retrieves the forst first index in the array
+		houseInfo.textContent = houseData[0];
 
 		video.src = `video/House-${targetSrc}.mp4`;
 
@@ -42,8 +52,8 @@
 		TweenMax.to(banners, 0.8, { right: totalOffset });
 	}
 
-	// shields.forEach(shield => shield.addEventListener('click', showLightbox));
-	shields.forEach(shield => shield.addEventListener('click', animateBanner));
+	shields.forEach(shield => shield.addEventListener('click', showLightbox));
+	// shields.forEach(shield => shield.addEventListener('click', animateBanner));
 	video.addEventListener('ended', hideLightbox);
 	closeLB.addEventListener('click', hideLightbox);
 })();
